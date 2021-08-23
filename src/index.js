ecaa7fed3c89faab421a22f2d56233cea12a531b
Luis Fonseca <01luisfonseca@gmail.com>
2021-08-23 17:47:14 -0500

    Update dependencies and babel

--- src/index.js
+++ src/index.js
@@ -4,62 +4,72 @@
  * MIT Licensed
  */
 
-function convertAsyncToURL (data) {
+const resizeImageWithCanvas = require("canvas-image-resizer");
+
+function convertAsyncToURL(data) {
   return new Promise((resolve, reject) => {
-    var reader = new FileReader()
-    reader.readAsDataURL(data)
-    reader.onloadend = () => resolve(reader.result)
-    reader.onerror = (err) => reject(err)
-    reader.onabort = () => reject(new Error('Abort URL convertion'))
-  })
+    var reader = new FileReader();
+    reader.readAsDataURL(data);
+    reader.onloadend = () => resolve(reader.result);
+    reader.onerror = (err) => reject(err);
+    reader.onabort = () => reject(new Error("Abort URL convertion"));
+  });
 }
 
-function base64ToFile (dataURI, paramsFile = {name: 'file'}) {
+function base64ToFile(dataURI, paramsFile = { name: "file" }) {
   var byteString, mimestring;
 
-  if(dataURI.split(',')[0].indexOf('base64') !== -1 ) {
-    byteString = atob(dataURI.split(',')[1]);
+  if (dataURI.split(",")[0].indexOf("base64") !== -1) {
+    byteString = atob(dataURI.split(",")[1]);
   } else {
-    byteString = decodeURI(dataURI.split(',')[1]);
+    byteString = decodeURI(dataURI.split(",")[1]);
   }
 
-  mimestring = dataURI.split(',')[0].split(':')[1].split(';')[0];
+  mimestring = dataURI.split(",")[0].split(":")[1].split(";")[0];
 
   var content = new Array();
   for (var i = 0; i < byteString.length; i++) {
     content[i] = byteString.charCodeAt(i);
   }
 
-  var newFile = new File(
-    [new Uint8Array(content)], paramsFile.name, {type: mimestring}
-  );
+  var newFile = new File([new Uint8Array(content)], paramsFile.name, {
+    type: mimestring,
+  });
 
   return newFile;
 }
 
-function imageCreator (url) {
+function imageCreator(url) {
   return new Promise((resolve, reject) => {
     var origImg = new Image();
     origImg.src = url;
     origImg.addEventListener("load", () => resolve(origImg));
-    origImg.onabort = () => reject('Abort Image')
-    origImg.onerror = reject
-  })
+    origImg.onabort = () => reject("Abort Image");
+    origImg.onerror = reject;
+  });
 }
 
-module.exports = function resizeFileImageWithCanvas (file, params = {}) {
+module.exports = function resizeFileImageWithCanvas(file, params = {}) {
   if (
-    (params?.width && !typeof params?.width === "number" && params?.width <= 0) ||
-    (params?.height && !typeof params?.height === "number" && params?.height <= 0) ||
-    (params?.quality && !typeof params?.quality === "number" && (params?.quality < 0 || params?.quality > 1))
+    (params?.width &&
+      !typeof params?.width === "number" &&
+      params?.width <= 0) ||
+    (params?.height &&
+      !typeof params?.height === "number" &&
+      params?.height <= 0) ||
+    (params?.quality &&
+      !typeof params?.quality === "number" &&
+      (params?.quality < 0 || params?.quality > 1))
   )
-    throw new Error("width and height must be positive integers, and quality must be a decimal with values into 0 and 1")
+    throw new Error(
+      "width and height must be positive integers, and quality must be a decimal with values into 0 and 1"
+    );
   let b64;
   return convertAsyncToURL(file)
-    .then(dt => imageCreator(dt))
-    .then(img => { 
+    .then((dt) => imageCreator(dt))
+    .then((img) => {
       b64 = img;
-      return base64ToFile(resizeImageWithCanvas(img, params), file)
+      return base64ToFile(resizeImageWithCanvas(img, params), file);
     })
-    .then(newfile => ({b64, file:newfile}));
+    .then((newfile) => ({ b64, file: newfile }));
 };
